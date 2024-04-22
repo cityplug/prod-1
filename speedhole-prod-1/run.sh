65a8f1f36a59bdbfd75996956a4a6df53ec9859f
cityplug <seni@cityplug.co.uk>
2024-04-22 19:28:55 +0100

    .

--- speedhole-prod-1/run.sh
+++ speedhole-prod-1/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Debian 8GB - (homepage-prod-1) setup script - pve thincentre {102}
+# Debian 8GB - (speedhole-prod-1) setup script - pve thincentre {101}
 
 #> systemctl mask ssh.socket && systemctl mask sshd.socket && systemctl disable sshd && systemctl enable ssh && sed -i '15i\Port 4792\n' /etc/ssh/sshd_config
 #> apt update -y && apt install git curl gnupg -y && apt full-upgrade -y && apt autoremove && reboot
-#> cd /opt && git clone https://github.com/cityplug/prod-1 && mv /opt/prod-1/homepage-prod-1 /opt/homepage-prod-1
-#> chmod +x /opt/homepage-prod-1/* && cd /opt/homepage-prod-1 && ./run.sh
+#> cd /opt && git clone https://github.com/cityplug/prod-1 && mv /opt/prod-1/speedhole-prod-1 /opt/speedhole-prod-1
+#> chmod +x /opt/speedhole-prod-1/* && cd /opt/speedhole-prod-1 && ./run.sh
 
 # --- Install Docker Official GPG key to Apt sources:
 install -m 0755 -d /etc/apt/keyrings
@@ -20,28 +20,20 @@ apt-get update && apt-get install docker-ce docker-ce-cli containerd.io docker-b
 
 # --- Addons
 rm -rf /etc/update-motd.d/* && rm -rf /etc/motd && 
-wget https://raw.githubusercontent.com/cityplug/prod-1/main/homepage-prod-1/10-uname -O /etc/update-motd.d/10-uname && chmod +x /etc/update-motd.d/10-uname
+wget https://raw.githubusercontent.com/cityplug/prod-1/main/speedhole-prod-1/10-uname -O /etc/update-motd.d/10-uname && chmod +x /etc/update-motd.d/10-uname
 
 echo "
 net.ipv4.ip_forward = 1
 net.ipv6.conf.all.forwarding = 1" >> /etc/sysctl.conf
 
-adduser focal
-usermod -aG docker focal
 #--
 systemctl enable docker 
 docker-compose --version && docker --version
-cd /opt/homepage-prod-1/ && docker-compose up -d && docker ps
+cd /opt/speedhole-prod-1/ && docker-compose up -d && docker ps
 docker-compose logs -f
 
 #--------------------------------------------------------------------------------
 reboot
-
-# --- Build Homepage
-#docker stop homepage
-#rm -rf /opt/appdata/homepage/
-#mv /opt/homepage/config /env/appdata/homepage
-#docker start homepage
 
 # --- Install Docker & Docker Compose 
 #curl -sSL https://get.docker.com/ | sh && apt install docker-compose -y
